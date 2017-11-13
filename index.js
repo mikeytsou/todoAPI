ec04e196b7e8e0888f3170ff84a1790dbc3fd4fa
Mike Tsou <mr.tsou@gmail.com>
2017-11-12 20:26:05 -0800

    Create todo model

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-      mongoose = require('mongoose');
       app = express();
 
-// MONGO SETUP
-const url = process.env.DATABASEURL || 'mongodb://localhost/todo_api';
-mongoose.set('debug', true);
-mongoose.connect(url);
-mongoose.Promise = Promise;
-
+app.get('*', (req, res) => {
+  res.send('PAGE NOT FOUND');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('CONNECTED TO PORT 3000');
