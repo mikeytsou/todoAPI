bd626928495b6bb39192130be3646b5e6b3700fe
Mike Tsou <mr.tsou@gmail.com>
2017-12-05 12:12:37 -0800

    Implement createTodo function that posts new todos

--- public/script.js
+++ public/script.js
@@ -1,21 +1,49 @@
 $(document).ready(function() {
   getTodos();
+  postTodos();
 });
 
 const getTodos = function() {
   $.getJSON('/api/todos')
   .then(addTodos)
-  .catch()
+  .catch(function(error) {
+    console.log('ERROR: ', error);
+  })
+}
 
-  function addTodos(todos) {
-    todos.forEach(function(todo) {
-      let newTodo = $(`<li class="task">${todo.description}</li>`);
+const postTodos = function() {
+  $('#todoInput').keypress(function(event) {
+    if(event.which === 13) {
+      createTodo();
+    }
+  });
 
-      if(todo.completed) {
-        newTodo.addClass('done')
-      }
+  function createTodo() {
+    let userInput = $('#todoInput').val();
 
-      $('.list').append(newTodo);
-    });
+    $.post('/api/todos', {description: userInput})
+    .then(function(newTodo) {
+      $('#todoInput').val('');
+      addTodo(newTodo);
+    })
+    .catch(function(error) {
+      console.log('ERROR: ', error)
+    })
   }
+}
+
+function addTodos(todos) {
+  todos.forEach(function(todo) {
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo) {
+  let newTodo = $(`<li class="task">${todo.description}</li>`);
+
+  if(todo.completed) {
+    newTodo.addClass('done')
+  }
+
+  $('.list').append(newTodo);
 }
