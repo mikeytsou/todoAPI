ffd7cb795c37c49c5c7ff6103c95f2aa0ba53c89
Mike Tsou <mr.tsou@gmail.com>
2017-12-04 21:28:24 -0800

    Update environment variable

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-// 'mongodb://localhost/todo_api';
 // 'mongodb://mtsou:todoapi@ds129776.mlab.com:29776/todo_api';
-const url = process.env.DATABASEURL;
+const url = process.env.DATABASEURL || 'mongodb://localhost/todo_api';
 mongoose.set('debug', true);
 mongoose.connect(url);
 mongoose.Promise = Promise;
