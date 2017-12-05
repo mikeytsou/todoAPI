bdbae1c94c960d83aa1f21b34a039b49309b647d
Mike Tsou <mr.tsou@gmail.com>
2017-12-04 21:08:47 -0800

    Implement mlab database for heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = process.env.DATABASEURL || 'mongodb://localhost/todo_api';
+// const url = process.env.DATABASEURL || 'mongodb://localhost/todo_api';
+const url = process.env.DATABASEURL || 'mongodb://mtsou:todoapi@ds129776.mlab.com:29776/todo_api';
+// mongodb://mtsou:todoapi@ds129776.mlab.com:29776/todo_api
 mongoose.set('debug', true);
 mongoose.connect(url);
 mongoose.Promise = Promise;
