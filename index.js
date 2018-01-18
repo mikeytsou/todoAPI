795584de34875835748df7b27be8e1f7f3832ccd
Mike Tsou <mr.tsou@gmail.com>
2018-01-18 00:06:49 -0800

    Update cors settings

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
       todoRoutes = require('./routes/todos');
 
 // APP CONFIG
-app.use(cors);
+// app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(`${__dirname}/public`));
